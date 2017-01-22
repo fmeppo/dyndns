6c309b58c48bf0ddc4198d09d0160cdcba8487fa
Mike Shuey <shuey@fmepnet.org>
2017-01-22 15:45:25 -0500

    Fix up line wrap.

--- connect.py
+++ connect.py
@@ -5,10 +5,14 @@ import urllib2, argparse, pprint
 
 parser = argparse.ArgumentParser(description='Update DNS record dynamically')
 parser.add_argument('fqdn', type=str, nargs=1, help='DNS record to update')
-parser.add_argument('-u', '--url', nargs=1, type=str, required=True, help='URL for the AWS API Gateway to use for updates')
-parser.add_argument('-t', '--token', nargs=1, type=str, required=True, help='Security token')
-parser.add_argument('-4', '--ipv4', nargs=1, type=str, help='IPv4 address to advertise (rather than autodetect)')
-parser.add_argument('-6', '--ipv6', nargs=1, type=str, help='IPv6 address to advertise (will not be autodetected)')
+parser.add_argument('-u', '--url', nargs=1, type=str, required=True,
+                    help='URL for the AWS API Gateway to use for updates')
+parser.add_argument('-t', '--token', nargs=1, type=str, required=True,
+                    help='Security token')
+parser.add_argument('-4', '--ipv4', nargs=1, type=str,
+                    help='IPv4 address to advertise (rather than autodetect)')
+parser.add_argument('-6', '--ipv6', nargs=1, type=str,
+                    help='IPv6 address to advertise (will not be autodetected)')
 
 args = parser.parse_args()
 
