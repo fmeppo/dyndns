2912bc1fd35d4ff319a1b5fe472e4518e7f415ea
Mike Shuey <shuey@fmepnet.org>
2017-02-12 09:22:03 -0500

    Process a config file for defaults.

--- connect.py
+++ connect.py
@@ -1,6 +1,6 @@
 #!/usr/local/bin/python
 
-import urllib2, argparse, pprint, sys
+import urllib2, argparse, pprint, sys, ConfigParser
 
 
 parser = argparse.ArgumentParser(description='Update DNS record dynamically')
@@ -15,11 +15,34 @@ parser.add_argument('-6', '--ipv6', nargs=1, type=str,
                     help='IPv6 address to advertise (will not be autodetected)')
 parser.add_argument('-v', '--verbose', action='store_true', dest='verbose',
                     help='Display verbose results from the server')
+parser.add_argument('-f', '--config', nargs=1, type=str, dest='config',
+                    help='Specify a config file with default options')
 
 args = parser.parse_args()
 
 vargs = vars(args)
+
+if vargs['config'] != None:
+    conffile = [ vargs['config'] ]
+else:
+    conffile = ['/usr/local/etc/dyndns.conf', '/etc/dyndns.con']
+
+config = ConfigParser.RawConfigParser()
+for f in conffile:
+    config.read(f)
+
 query_args = ['fqdn','token','ipv4','ipv6']
+
+arghash = {}
+for item in query_args:
+    if vargs[item] != None:
+        arghash[item] = vargs[item]
+    else:
+        try:
+            arghash[item] = config.get(vargs['fqdn'], item)
+        except Exception, e:
+            arghash[item] = None
+
 qstring = reduce(lambda x, y: x + '&' + y,  \
             list(map(lambda x: x + '=' + str(vargs[x][0]), \
             list(filter(lambda x: vargs[x] != None, query_args)) )) )
