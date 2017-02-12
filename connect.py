c75846cd309862969220a536cdcd57b601e3fe33
Mike Shuey <shuey@fmepnet.org>
2017-02-12 11:17:25 -0500

    Allow multiple hosts to be specified.
    Use the config file for defaults for anything not on the command line.

--- connect.py
+++ connect.py
@@ -5,9 +5,9 @@ import urllib2, argparse, pprint, sys, ConfigParser
 
 parser = argparse.ArgumentParser(description='Update DNS record dynamically')
 parser.add_argument('fqdn', type=str, nargs=1, help='DNS record to update')
-parser.add_argument('-u', '--url', nargs=1, type=str, required=True,
+parser.add_argument('-u', '--url', nargs=1, type=str,
                     help='URL for the AWS API Gateway to use for updates')
-parser.add_argument('-t', '--token', nargs=1, type=str, required=True,
+parser.add_argument('-t', '--token', nargs=1, type=str,
                     help='Security token')
 parser.add_argument('-4', '--ipv4', nargs=1, type=str,
                     help='IPv4 address to advertise (rather than autodetect)')
@@ -31,42 +31,57 @@ config = ConfigParser.RawConfigParser()
 for f in conffile:
     config.read(f)
 
-query_args = ['fqdn','token','ipv4','ipv6']
+for host in vargs['fqdn']:
+    query_args = ['fqdn','token','ipv4','ipv6']
 
-arghash = {}
-for item in query_args:
-    if vargs[item] != None:
-        arghash[item] = vargs[item]
+    # fqdn is fixed (it's the host we're iterating on).  The rest can come
+    # from either the command line (vargs) or the config file, or be None
+    # if no value is provided.
+    arghash = {'fqdn': host}
+    for item in ['url'] + query_args[1:]:
+        if vargs[item] != None:
+            arghash[item] = vargs[item]
+        else:
+            try:
+                arghash[item] = config.get(host, item)
+            except Exception, e:
+                arghash[item] = None
+
+    # Since token and url can come from a config file or command line, we
+    # need to validate we have these for this host.
+    if arghash['token'] == None:
+        print "Missing security token for " + host
+        sys.exit(1)
+    if arghash['url'] == None:
+        print "Missing URL for " + host
+        sys.exit(2)
+
+    # Collect non-null arguments to be passed via query string, and format
+    # the query string appropriately.
+    qstring = reduce(lambda x, y: x + '&' + y,  \
+                list(map(lambda x: x + '=' + str(vargs[x][0]), \
+                list(filter(lambda x: vargs[x] != None, query_args)) )) )
+
+    try:
+        u = urllib2.urlopen(arghash['url'] + '?' + qstring)
+        data = u.read()
+        u.close()
+    except urllib2.HTTPError, e:
+        print "HTTP error: " + str(e)
+        sys.exit(1)
+    except urllib2.URLError, e:
+        print "Network error: " + str(e)
+        sys.exit(2)
+    except ValueError, e:
+        print "Invalid URL: " + args.url[0]
+        sys.exit(3)
+
+    if args.verbose:
+        print data
+
+    if u.getcode() == 200:
+        # Success!
+        sys.exit(0)
     else:
-        try:
-            arghash[item] = config.get(vargs['fqdn'], item)
-        except Exception, e:
-            arghash[item] = None
-
-qstring = reduce(lambda x, y: x + '&' + y,  \
-            list(map(lambda x: x + '=' + str(vargs[x][0]), \
-            list(filter(lambda x: vargs[x] != None, query_args)) )) )
-
-try:
-    u = urllib2.urlopen(args.url[0] + '?' + qstring)
-    data = u.read()
-    u.close()
-except urllib2.HTTPError, e:
-    print "HTTP error: " + str(e)
-    sys.exit(1)
-except urllib2.URLError, e:
-    print "Network error: " + str(e)
-    sys.exit(2)
-except ValueError, e:
-    print "Invalid URL: " + args.url[0]
-    sys.exit(3)
-
-if args.verbose:
-    print data
-
-if u.getcode() == 200:
-    # Success!
-    sys.exit(0)
-else:
-    print "Failed to set DNS records."
-    sys.exit(1)
+        print "Failed to set DNS records."
+        sys.exit(1)
